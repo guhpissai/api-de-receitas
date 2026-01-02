8610755c0a29b23ac78fd16442c65ca47d30b8d7
guhpissai <gustavopissai3d@gmail.com>
2026-01-02 18:33:54 -0300

    feat: Requisito 1, 2, 3 | Implementa as rotas GET /recipe e recipe/:name e a rota POST

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -12,40 +12,45 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("recipe")]
 public class RecipesController : ControllerBase
-{    
+{
     public readonly IRecipeService _service;
-    
+
     public RecipesController(IRecipeService service)
     {
-        this._service = service;        
+        this._service = service;
     }
 
     // 1 - Sua aplicação deve ter o endpoint GET /recipe
     //Read
     [HttpGet]
     public IActionResult Get()
-    {
-        throw new NotImplementedException();    
-    }
+     => Ok(_service.GetRecipes());
 
     // 2 - Sua aplicação deve ter o endpoint GET /recipe/:name
     //Read
     [HttpGet("{name}", Name = "GetRecipe")]
     public IActionResult Get(string name)
-    {                
-        throw new NotImplementedException();
+    {
+        var recipe = _service.GetRecipe(name);
+
+        if (recipe == null)
+            return NotFound();
+
+        return Ok(recipe);
     }
 
     // 3 - Sua aplicação deve ter o endpoint POST /recipe
     [HttpPost]
-    public IActionResult Create([FromBody]Recipe recipe)
+    public IActionResult Create([FromBody] Recipe recipe)
     {
-        throw new NotImplementedException();
+        _service.AddRecipe(recipe);
+
+        return CreatedAtAction("GetRecipe", new { name = recipe.Name }, recipe);
     }
 
     // 4 - Sua aplicação deve ter o endpoint PUT /recipe
     [HttpPut("{name}")]
-    public IActionResult Update(string name, [FromBody]Recipe recipe)
+    public IActionResult Update(string name, [FromBody] Recipe recipe)
     {
         throw new NotImplementedException();
     }
@@ -55,5 +60,5 @@ public class RecipesController : ControllerBase
     public IActionResult Delete(string name)
     {
         throw new NotImplementedException();
-    }    
+    }
 }
